051218ba56b621f6b04f44c68b794013d688b6f7
epimoni <45078821+DaveeGaming@users.noreply.github.com>
2024-06-25 20:57:35 +0200

    Basic main file and wasm build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("title")]
+async fn main() {
+    loop {
+        draw_text("Hello world!", 100.0, 100.0, 30.0, WHITE);
+        next_frame().await;
+    }
 }
