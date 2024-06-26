b73501f5c9d35570cecdbcbe9657c820abff3474
epimoni <45078821+DaveeGaming@users.noreply.github.com>
2024-06-26 09:24:24 +0200

    Fixed window scaling

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
 
+// FEATURE CREEP CORNER
+// Blue/Red shio/enemy/bullet
+// Different palettes
+// Weapon for diff color ships
+// Bullethell kinda stuff
+// Fast mouse movement vs asteroid movement vs any other idk
+
+
 use macroquad::prelude::*;
-use miniquad::window::screen_size;
+use miniquad::{log, window::screen_size};
 
 const DESIGN_WIDTH: f32 = 1024.;
 const DESIGN_HEIGHT: f32 = 576.;
-
 #[macroquad::main("title")]
 async fn main() {
     let canvas = render_target(DESIGN_WIDTH as u32, DESIGN_HEIGHT as u32);
-
+    canvas.texture.set_filter(FilterMode::Nearest);
     loop {
         let (screen_w, screen_h) = screen_size();
 
         let scale = f32::min(screen_w / DESIGN_WIDTH, screen_h / DESIGN_HEIGHT);
 
+        let x_center = (screen_w - DESIGN_WIDTH * scale) / 2.0;
+        let y_center = (screen_h - DESIGN_HEIGHT * scale) / 2.0;
+
         let mut camera = Camera2D::from_display_rect(Rect {
             x: 0.,
             y: 0.,
-            w: (DESIGN_WIDTH * scale) as f32,
-            h: (DESIGN_HEIGHT * scale) as f32   
+            w: (DESIGN_WIDTH) as f32,
+            h: (DESIGN_HEIGHT) as f32   
         });
 
+        camera.render_target = Some(canvas.clone());
         camera.zoom.y = -camera.zoom.y;
         
         set_camera(&camera);
@@ -31,7 +42,12 @@ async fn main() {
         
         set_default_camera();
         clear_background(BLANK);
-        draw_texture(&canvas.texture, 0., 0., WHITE);
+        draw_texture_ex(&canvas.texture, x_center, y_center, WHITE, 
+            DrawTextureParams {
+                dest_size: Some( Vec2 { x: DESIGN_WIDTH * scale, y: DESIGN_HEIGHT * scale  }),
+                ..Default::default()
+            }
+        );
         
         next_frame().await
     }
