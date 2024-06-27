8b8c7239ce8a138cc56ff2b5feebcbba2599aabc
doczi-dominik <doczi.dominik0@gmail.com>
2024-06-27 12:54:24 +0200

    melee circ

--- src/player.rs
+++ src/player.rs
@@ -28,7 +28,8 @@ pub struct Player {
     shoot_dx: f32,
     shoot_dy: f32,
     bullets: Vec<PlayerBullet>,
-    shoot_t: u32
+    shoot_t: u32,
+    melee_t: u32
 }
 
 impl Default for Player {
@@ -41,10 +42,11 @@ impl Default for Player {
             y: 50.0,
             dx: 0.0,
             dy: 0.0,
-            shoot_dx: 0.0,
+            shoot_dx: 1.0,
             shoot_dy: 0.0,
             bullets: vec![],
-            shoot_t: 0
+            shoot_t: 0,
+            melee_t: 0
         }
     }
 }
@@ -56,8 +58,15 @@ impl Player {
             ColorState::Secondary => c.FG_SECONDARY
         };
 
+        let center_x = self.x + 15.0;
+        let center_y = self.y + 15.0;
+
         for b in &self.bullets {
             b.draw();
+        }
+
+        if self.melee_t > 0 && self.melee_t < 15 {
+            draw_circle(center_x, center_y, 45.0, YELLOW);
         }
 
         draw_rectangle_ex(self.x, self.y, 30.0, 30.0,
@@ -91,16 +100,10 @@ impl Player {
             dy = 1.0;
         }
 
-        self.dx = dx * speed;
-        self.dy = dy * speed;
+        if dx != 0.0 { self.dx = dx * speed; }
+        if dy != 0.0 { self.dy = dy * speed; }
 
-        if !is_key_down(KeyCode::J) {
-            self.shoot_dx = dx;
-            self.shoot_dy = dy; 
-
-            self.shoot_t = 0;
-        } else {
-
+        if is_key_down(KeyCode::J) || is_key_down(KeyCode::F) {
             if self.shoot_t % 5 == 0 {
                 self.bullets.push(PlayerBullet {
                     x: self.x + 15.0,
@@ -112,6 +115,21 @@ impl Player {
             }
 
             self.shoot_t += 1;
+        } else {
+            if dx != 0.0 || dy != 0.0 {
+                self.shoot_dx = dx;
+                self.shoot_dy = dy; 
+            }
+
+            self.shoot_t = 0;
+        }
+
+        if self.melee_t == 0 && (is_key_down(KeyCode::K) || is_key_down(KeyCode::G)) {
+            self.melee_t = 23;
+        }
+
+        if self.melee_t > 0 {
+            self.melee_t -= 1;
         }
 
         self.x += self.dx * dt;
